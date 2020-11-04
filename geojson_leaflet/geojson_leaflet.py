1b064ba51635c5d7e7cb120f714213ed048db662
Stefan Hagen <stefan@hagen.link>
2020-11-04 20:32:34 +0100

    Update geojson_leaflet.py

--- geojson_leaflet/geojson_leaflet.py
+++ geojson_leaflet/geojson_leaflet.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 from geojson import Feature, Point, FeatureCollection
+from geojson import dumps as geojson_dumps
 
 DEBUG_VAR = "LIAISON_DEBUG"
 DEBUG = os.getenv(DEBUG_VAR)
@@ -22,12 +23,22 @@ def main(argv=None):
     obj = Point((3.1415, 42))
     if not obj.is_valid:
         return 1
-    my_feature = Feature(geometry=Point((1.6432, -19.123)))
-    my_other_feature = Feature(geometry=Point((-80.234, -22.532)))
-    feature_collection = FeatureCollection([my_feature, my_other_feature])
+    my_feature = Feature(
+        geometry=Point((1.6432, -19.123)),
+        properties = {"name": "<a href='abc' target='_blank'>abc</a>"}
+    )
+    my_other_feature = Feature(
+        geometry=Point((-80.234, -22.532)),
+        properties = {"name": "<a href='def' target='_blank'>def</a>"}
+    )
+    feature_collection = FeatureCollection(
+        [my_feature, my_other_feature],
+        name="test-points-link-labels",
+        crs={"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}})
     if not feature_collection:
         return 1
     if feature_collection.errors():
         return 1
     assert all([feature_collection[0] == feature_collection['features'][0], feature_collection[1] == my_other_feature]) is True
+    DEBUG and print(geojson_dumps(feature_collection, indent=2))
     return 0
