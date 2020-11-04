d830f2a61aba28a326ca2b7379d923463d627be3
Stefan Hagen <stefan@hagen.link>
2020-11-04 19:29:46 +0100

    Update cli.py

--- geojson_leaflet/cli.py
+++ geojson_leaflet/cli.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=line-too-long
 """"Generate geojson data based leaflet driven web app from flat data files."""
-import os
 import sys
 
 import geojson_leaflet.geojson_leaflet as liaison
@@ -10,6 +9,6 @@ import geojson_leaflet.geojson_leaflet as liaison
 
 # pylint: disable=expression-not-assigned
 def main(argv=None):
-    """Process the files separately per folder."""
+    """Delegate processing to functional module."""
     argv = sys.argv[1:] if argv is None else argv
     liaison.main(argv)
