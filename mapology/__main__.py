d1d456ccd9800150aa519bc2cbfdc834b1e4722a
Stefan Hagen <stefan@hagen.link>
2021-05-15 17:38:28 +0200

    Linted main

--- mapology/__main__.py
+++ mapology/__main__.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-docstring
 import sys
 
-from geojson_leaflet.cli import main
+from mapology.cli import main
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))  # pragma: no cover
