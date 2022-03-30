bece94be64d0b9a1576fefeb93bb3a08eb1c8850
Stefan Hagen <stefan@hagen.link>
2022-03-30 18:58:27 +0200

    Happy linter, happy whatever

--- mapology/r_filter.py
+++ mapology/r_filter.py
@@ -17,14 +17,14 @@ import logging
 import os
 import pathlib
 import sys
-from typing import Callable, Collection, Dict, Iterator, List, Tuple, Union, no_type_check
+from typing import Callable, Collection, Dict, Iterator, List, Mapping, Tuple, Union, no_type_check
 
 FeatureDict = Dict[str, Collection[str]]
 PHeaderDict = Dict[str, Collection[str]]
 PFeatureDict = Dict[str, Collection[str]]
 
 ENCODING = 'utf-8'
-THIS_YY_INT = int(dti.datetime.utcnow().strftime("%y"))
+THIS_YY_INT = int(dti.datetime.utcnow().strftime('%y'))
 
 COUNTRY_PAGE = os.getenv('GEO_COUNTRY_PAGE', '')
 PATH_NAV = os.getenv('GEO_PATH_NAV', '')
@@ -32,7 +32,6 @@ BASE_URL = os.getenv('GEO_BASE_URL', 'http://localhost:8080')
 AERONAUTICAL_ANNOTATIONS = os.getenv('GEO_PRIMARY_LAYER_SWITCH', 'Airports')
 
 FS_PREFIX_PATH = os.getenv('GEO_PREFIX_PATH', 'prefix')
-DERIVE_GEOJSON_NAME = 'derive'
 
 REC_SEP = ','
 STDIN_TOKEN = '-'
@@ -124,7 +123,8 @@ GEO_JSON_HEADER = {
 GEO_JSON_FEATURE: FeatureDict = {
     'type': 'Feature',
     'properties': {
-        'name': f"<a href='{URL}' class='nd' target='_blank' title='{KIND} {ITEM} of {ICAO}({CITY}, {CC_HINT})'>{TEXT}</a>",
+        'name':
+            f"<a href='{URL}' class='nd' target='_blank' title='{KIND} {ITEM} of {ICAO}({CITY}, {CC_HINT})'>{TEXT}</a>",
     },
     'geometry': {
         'type': 'Point',
@@ -218,7 +218,7 @@ if PREFIX_TABLE_STORE.exists() and PREFIX_TABLE_STORE.is_file() and PREFIX_TABLE
         prefix_table_store = json.load(handle)
 
 
-def derive_base_facts_path(folder: pathlib.Path, icao_identifier:str) -> pathlib.Path:
+def derive_base_facts_path(folder: pathlib.Path, icao_identifier: str) -> pathlib.Path:
     """DRY."""
     return pathlib.Path(folder, f'airport-{icao_identifier.upper()}.json')
 
@@ -228,14 +228,14 @@ def derive_geojson_in_path(folder: pathlib.Path, icao_identifier: str) -> pathli
     return pathlib.Path(folder, f'airport-{icao_identifier.upper()}.geojson')
 
 
-def parse_cycle_date(cycle_date_code:str) -> list[int, int]:
+def parse_cycle_date(cycle_date_code: str) -> list[int, int]:
     """Parse string encoded cycle date into full (YYYY, cycle number)- pair."""
     dt, cy = int(cycle_date_code[:2]), int(cycle_date_code[2:])
     dt = 1900 + dt if dt > THIS_YY_INT else 2000 + dt
     return [dt, cy]
 
 
-def parse_int_or_empty(decimals:str) -> Union[int, None]:
+def parse_int_or_empty(decimals: str) -> Union[int, None]:
     """Parse string encoded decimal and yield integer or None."""
     return None if not decimals.strip() else int(decimals.strip())
 
@@ -249,35 +249,35 @@ def parse_base_facts(folder: pathlib.Path, icao_identifier: str) -> dict[str, Un
 
     and may correct the prefix semantics in the page for prefix to country mapping.
     """
-    with open(derive_base_facts_path(folder, icao_identifier), "rt", encoding=ENCODING) as raw_handle:
+    with open(derive_base_facts_path(folder, icao_identifier), 'rt', encoding=ENCODING) as raw_handle:
         data = json.load(raw_handle)
-    conv = data["airport_converted"]
-    raw = data["airport_raw"]
+    conv = data['airport_converted']
+    raw = data['airport_raw']
     return {
-        "airport_name": raw["airport_name"].strip(),
-        "customer_area_code": raw["customer_area_code"].strip().upper(),  # "USA"
-        "icao_code": raw["icao_code"].strip().upper(),  # "K2"
-        "icao_identifier": raw["icao_identifier"].strip().upper(),  # "04CA"
-        "ifr_capability": raw["ifr_capability"].strip(),  # "N"
-        "longest_runway": raw["longest_runway"].strip(),  # "080"
-        "longest_runway_surface_code": raw["longest_runway_surface_code"].strip(),  # " " -> ''
-        "magnetic_true_indicator": raw["magnetic_true_indicator"].strip(),  # "M"
-        "magnetic_variation": raw["magnetic_variation"].strip(),  # "E0140"
-        "public_military_indicator": raw["public_military_indicator"].strip(),  # "P"
-        "recommended_navaid": raw["recommended_navaid"].strip(),  # "    " -> ''
-        "speed_limit": parse_int_or_empty(raw["speed_limit"]),  # in km/h
-        "speed_limit_altitude": raw["speed_limit_altitude"].strip(),  # in feet for speed limit
-        "time_zone": raw["time_zone"].strip(),  # "U00" or similarly
-        "transition_level": parse_int_or_empty(raw["transition_level"]),  # in feet
-        "transitions_altitude": parse_int_or_empty(raw["transitions_altitude"]),  # in feet
-        "latitude": conv["latitude"],  # signed degrees as float
-        "longitude": conv["longitude"],  # signed degrees as float
-        "elevation": conv["elevation"],  # meters above mean sea level as float
-        "record_type": raw["record_type"].strip().upper(),
-        "section_code": raw["section_code"].strip().upper(),
-        "subsection_code": raw["subsection_code"].strip().upper(),
-        "cycle_date": parse_cycle_date(raw["cycle_date"]),  # Code "1913" -> [2019, 13]
-        "file_record_number": int(raw["file_record_number"].strip()),  # Five digits wrap around counter within db
+        'airport_name': raw['airport_name'].strip(),
+        'customer_area_code': raw['customer_area_code'].strip().upper(),  # 'USA'
+        'icao_code': raw['icao_code'].strip().upper(),  # 'K2'
+        'icao_identifier': raw['icao_identifier'].strip().upper(),  # '04CA'
+        'ifr_capability': raw['ifr_capability'].strip(),  # 'N'
+        'longest_runway': raw['longest_runway'].strip(),  # '080'
+        'longest_runway_surface_code': raw['longest_runway_surface_code'].strip(),  # ' ' -> ''
+        'magnetic_true_indicator': raw['magnetic_true_indicator'].strip(),  # 'M'
+        'magnetic_variation': raw['magnetic_variation'].strip(),  # 'E0140'
+        'public_military_indicator': raw['public_military_indicator'].strip(),  # 'P'
+        'recommended_navaid': raw['recommended_navaid'].strip(),  # '    ' -> ''
+        'speed_limit': parse_int_or_empty(raw['speed_limit']),  # in km/h
+        'speed_limit_altitude': raw['speed_limit_altitude'].strip(),  # in feet for speed limit
+        'time_zone': raw['time_zone'].strip(),  # 'U00' or similarly
+        'transition_level': parse_int_or_empty(raw['transition_level']),  # in feet
+        'transitions_altitude': parse_int_or_empty(raw['transitions_altitude']),  # in feet
+        'latitude': conv['latitude'],  # signed degrees as float
+        'longitude': conv['longitude'],  # signed degrees as float
+        'elevation': conv['elevation'],  # meters above mean sea level as float
+        'record_type': raw['record_type'].strip().upper(),
+        'section_code': raw['section_code'].strip().upper(),
+        'subsection_code': raw['subsection_code'].strip().upper(),
+        'cycle_date': parse_cycle_date(raw['cycle_date']),  # Code '1913' -> [2019, 13]
+        'file_record_number': int(raw['file_record_number'].strip()),  # Five digits wrap around counter within db
     }
 
 
@@ -384,7 +384,7 @@ def parse_data(reader: Callable[[], Iterator[str]]) -> Tuple[Dict[str, bool], Di
     return seen, data, lines
 
 
-def make_feature(coord_stack: Dict[Tuple[str, str], int], feature_data: List[Point], kind: str, cc: str, icao: str, apn: str) -> List[FeatureDict]:
+def collect_glideslopes(feature_data: List[Point]) -> Mapping[str: object]:
     """DRY."""
     glideslopes = {}
     for triplet in feature_data:
@@ -398,6 +398,15 @@ def make_feature(coord_stack: Dict[Tuple[str, str], int], feature_data: List[Poi
             glideslopes[pair]['local_id'] = label[:-4]
             glideslopes[pair]['kinds'].append(label[-3:])
             glideslopes[pair]['kinds'].sort()
+
+    return glideslopes
+
+
+def make_feature(
+    coord_stack: Dict[Tuple[str, str], int], feature_data: List[Point], kind: str, cc: str, icao: str, apn: str
+) -> List[FeatureDict]:
+    """DRY."""
+    glideslopes = collect_glideslopes(feature_data)
 
     local_features = []
     for triplet in feature_data:
@@ -419,7 +428,11 @@ def make_feature(coord_stack: Dict[Tuple[str, str], int], feature_data: List[Poi
                     name = name.replace(ITEM, label_display).replace(TEXT, label_display)
                     name = name.replace(CITY, apn)
                     name = name.replace(CC_HINT, cc)
-                    name = '<small>' + name.replace(URL, GOOGLE_MAPS_URL.format(lat=float(lat_str), lon=float(lon_str))) + '</small>'
+                    name = (
+                        '<small>'
+                        + name.replace(URL, GOOGLE_MAPS_URL.format(lat=float(lat_str), lon=float(lon_str)))
+                        + '</small>'
+                    )
                 else:
                     name = None
             else:
@@ -451,7 +464,11 @@ def make_feature(coord_stack: Dict[Tuple[str, str], int], feature_data: List[Poi
             name = name.replace(ITEM, label_display).replace(TEXT, label_display)
             name = name.replace(CITY, apn)
             name = name.replace(CC_HINT, cc)
-            name = '<small>' + name.replace(URL, GOOGLE_MAPS_URL.format(lat=float(lat_str), lon=float(lon_str))) + '</small>'
+            name = (
+                '<small>'
+                + name.replace(URL, GOOGLE_MAPS_URL.format(lat=float(lat_str), lon=float(lon_str)))
+                + '</small>'
+            )
         else:
             name = feature['properties']['name']  # type: ignore
             name = name.replace(ICAO, icao).replace(KIND, kind)
@@ -523,7 +540,7 @@ def make_table_row(facts):
     row = copy.deepcopy(JSON_PREFIX_TABLE_ROW)
     row['area_code'] = facts['customer_area_code']
     row['prefix'] = facts['icao_code']
-    row['icao'] = facts["icao_identifier"]
+    row['icao'] = facts['icao_identifier']
     row['latitude'] = facts['latitude']
     row['longitude'] = facts['longitude']
     row['elevation'] = facts['elevation']
@@ -548,18 +565,12 @@ def add_airport(point: Point, cc: str, icao: str, apn: str) -> PFeatureDict:
     return airport
 
 
-def main(argv: Union[List[str], None] = None) -> int:
-    """Drive the derivation."""
-    argv = sys.argv[1:] if argv is None else argv
-    if len(argv) != 1:
-        print('usage: r_filter.py base/r/IC[/ICAO]')
-        return 2
-
-    slash, magic = '/', '/r/'
-    bootstrap = argv[0].rstrip(slash)  # ensure it does not end with a slash
+def expand_tasks(text_path: str, path_sep: str, magic_token: str) -> List[str]:
+    """DRY."""
+    bootstrap = text_path.rstrip(path_sep)  # ensure it does not end with a slash
     # is now either where/ever/r or where/ever/r/CC or where/ever/r/CC/ICAO
-    full = bootstrap.endswith(magic.rstrip(slash))
-    cc_only = not full and slash not in bootstrap.rsplit(magic, 1)[1]
+    full = bootstrap.endswith(magic_token.rstrip(path_sep))
+    cc_only = not full and path_sep not in bootstrap.rsplit(magic_token, 1)[1]
     tasks = []
     if full:
         for path in pathlib.Path(bootstrap).iterdir():
@@ -573,6 +584,46 @@ def main(argv: Union[List[str], None] = None) -> int:
                 tasks.append(str(path))
     else:
         tasks.append(bootstrap)
+
+    return tasks
+
+
+def best_effort_cc_hint(a_prefix: str, some_facts: Mapping[str: object], lookup: Mapping[str: object]) -> str:
+    """DRY."""
+    try:
+        return lookup[a_prefix]
+    except KeyError as err:
+        log.debug('Naive prefix matcher failed falling back to raw data: %s' % str(err).replace('\n', '$NL$'))
+        log.debug('DETAILS: %s' % str(some_facts))
+        return lookup.get(some_facts.get('icao_code', 'ZZ'))
+
+
+def best_effort_prefix_path(a_prefix: str, a_root_icao: str, lookup: Mapping[str: object]) -> str:
+    """DRY."""
+    try:
+        return lookup[a_root_icao]
+    except KeyError as err:
+        log.debug(
+            'Naive prefix path matcher failed falling back to derivation: %s' % str(err).replace('\n', '$NL$')
+        )
+        return f'/prefix/{a_prefix}/{a_root_icao}/'
+
+
+def write_json_store(at: pathlib.Path, what: Mapping[:str, object]) -> None:
+    """DRY."""
+    with open(at, 'wt', encoding=ENCODING) as handle:
+        json.dump(what, handle, indent=2)
+
+
+def main(argv: Union[List[str], None] = None) -> int:
+    """Drive the derivation."""
+    argv = sys.argv[1:] if argv is None else argv
+    if len(argv) != 1:
+        print('usage: r_filter.py base/r/IC[/ICAO]')
+        return 2
+
+    slash, magic = '/', '/r/'
+    tasks = expand_tasks(argv[0], slash, magic)
 
     num_tasks = len(tasks)
     many = num_tasks > 4200  # hundredfold magic
@@ -591,21 +642,21 @@ def main(argv: Union[List[str], None] = None) -> int:
             triplet = data[AIRP][0]
             root_icao, root_lat, root_lon = triplet.label.strip(), float(triplet.lat), float(triplet.lon)
             facts = parse_base_facts(s_folder, root_icao)
-            s_name = facts["airport_name"]
-            s_area_code = facts["customer_area_code"]
-            s_prefix = facts["icao_code"]
+            s_name = facts['airport_name']
+            s_area_code = facts['customer_area_code']
+            s_prefix = facts['icao_code']
             ic_prefix = s_prefix  # HACK A DID ACK
 
             message = f'processing {current :>5d}/{num_tasks} {ic_prefix}/{root_icao} --> ({s_name}) ...'
             if not many or not current % 100 or current == num_tasks:
                 log.info(message)
 
-            s_identifier = facts["icao_identifier"]
-            s_lat = facts["latitude"]
-            s_lon = facts["longitude"]
-            s_elev = facts["elevation"]
+            s_identifier = facts['icao_identifier']
+            s_lat = facts['latitude']
+            s_lon = facts['longitude']
+            s_elev = facts['elevation']
             s_updated = f'{"/".join(str(f) for f in facts["cycle_date"])}'
-            s_rec_num = facts["file_record_number"]
+            s_rec_num = facts['file_record_number']
             geojson_path = derive_geojson_in_path(g_folder, s_identifier)
 
             if s_identifier not in airport_name:
@@ -627,18 +678,8 @@ def main(argv: Union[List[str], None] = None) -> int:
                 f'<td class="la">{s_name}</td></tr>'
             )
 
-            try:
-                cc_hint = flat_prefix[ic_prefix]
-            except KeyError as err:
-                log.debug("Naive prefix matcher failed falling back to raw data: %s" % str(err).replace("\n", "$NL$"))
-                log.debug("DETAILS: %s" % str(facts))
-                cc_hint = flat_prefix.get(facts.get("icao_code", "ZZ"))
-
-            try:
-                my_prefix_path = prefix_path[root_icao]
-            except KeyError as err:
-                log.debug("Naive prefix path matcher failed falling back to derivation: %s" % str(err).replace("\n", "$NL$"))
-                my_prefix_path = f'/prefix/{ic_prefix}/{root_icao}/'
+            cc_hint = best_effort_cc_hint(ic_prefix, facts, flat_prefix)
+            my_prefix_path = best_effort_prefix_path(ic_prefix, root_icao, prefix_path)
 
             markers = cc_hint, root_icao, s_name
 
@@ -656,7 +697,8 @@ def main(argv: Union[List[str], None] = None) -> int:
                 geojson['features'].extend(make_feature(coord_stack, data[LOCA], 'Localizer', *markers))  # type: ignore
 
             if GLID in data:
-                geojson['features'].extend(make_feature(coord_stack, data[GLID], 'Glideslope', *markers))  # type: ignore
+                geojson['features'].extend(  # type: ignore
+                    make_feature(coord_stack, data[GLID], 'Glideslope', *markers))
 
             # Process prefix store
             if ic_prefix not in prefix_store:
@@ -677,20 +719,14 @@ def main(argv: Union[List[str], None] = None) -> int:
             prefix_root = pathlib.Path(FS_PREFIX_PATH)
             map_folder = pathlib.Path(prefix_root, ic_prefix, root_icao)
             map_folder.mkdir(parents=True, exist_ok=True)
-            if geojson_path == DERIVE_GEOJSON_NAME:
-                geojson_path = str(pathlib.Path(map_folder, f'{root_icao.lower()}-geo.json'))
-            geo_json_name = pathlib.Path(geojson_path).name
-            with open(geojson_path, 'wt', encoding=ENCODING) as geojson_handle:
-                json.dump(geojson, geojson_handle, indent=2)
+            write_json_store(geojson_path, geojson)
             log.debug('Wrote geojson to %s' % str(geojson_path))
             geojson_path = str(pathlib.Path(map_folder, f'{root_icao.lower()}-geo.json'))
             geo_json_name = pathlib.Path(geojson_path).name
-            with open(geojson_path, 'wt', encoding=ENCODING) as geojson_handle:
-                json.dump(geojson, geojson_handle, indent=2)
+            write_json_store(pathlib.Path(geojson_path), geojson)
             log.debug('Wrote geojson to %s' % str(geojson_path))
             r_source_path = str(pathlib.Path(map_folder, f'airport-with-runways-{root_icao}.r'))
-            with open(r_source_path, 'wt', encoding=ENCODING) as r_source_handle:
-                json.dump(geojson, r_source_handle, indent=2)
+            write_json_store(pathlib.Path(r_source_path), geojson)
             log.debug('Wrote R Source to %s' % str(r_source_path))
 
             html_dict = {
@@ -724,11 +760,8 @@ def main(argv: Union[List[str], None] = None) -> int:
             with open(html_path, 'wt', encoding=ENCODING) as html_handle:
                 html_handle.write(html_page)
 
-            with open(PREFIX_TABLE_STORE, 'wt', encoding=ENCODING) as json_prefix_table_handle:
-                json.dump(prefix_table_store, json_prefix_table_handle, indent=2)
-
-            with open(PREFIX_STORE, 'wt', encoding=ENCODING) as geojson_prefix_handle:
-                json.dump(prefix_store, geojson_prefix_handle, indent=2)
+            write_json_store(PREFIX_TABLE_STORE, prefix_table_store)
+            write_json_store(PREFIX_STORE, prefix_store)
 
         else:
             log.warning('no airport found with R sources.')
