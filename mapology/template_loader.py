1a0045c9265b18d5b0a374c861dc78dbcccc5c44
Stefan Hagen <stefan@hagen.link>
2022-04-04 22:18:51 +0200

    Implemented the template eject API

--- mapology/template_loader.py
+++ mapology/template_loader.py
@@ -1,7 +1,14 @@
 """Loader function for templates."""
 import importlib.resources
+import pathlib
+from typing import List, Union
 
 ENCODING = 'utf-8'
+RESOURCES = (
+    'index_template.html',
+    'page_template.html',
+    'prefix_template.html',
+)
 
 
 def load_html(resource: str, is_complete_path: bool = False) -> str:
@@ -13,3 +20,21 @@ def load_html(resource: str, is_complete_path: bool = False) -> str:
         with importlib.resources.path(__package__, resource) as html_template_path:
             with open(html_template_path, 'rt', encoding=ENCODING) as handle:
                 return handle.read()
+
+
+def eject(argv: Union[List[str], None] = None) -> int:
+    """Eject the templates into the folder given (default EJECTED) and create the folder if it does not exist."""
+    argv = argv if argv else ['']
+    into = argv[0]
+    if not into.strip():
+        into = 'EJECTED'
+    into_path = pathlib.Path(into)
+    into_path.mkdir(parents=True, exist_ok=True)
+    for resource in RESOURCES:
+        write_to = into_path / resource
+        with importlib.resources.path(__package__, resource) as html_template_path:
+            with open(html_template_path, 'rt', encoding=ENCODING) as handle:
+                with open(write_to, 'wt', encoding=ENCODING) as target:
+                    target.write(handle.read())
+
+    return 0
