cb4184237ce90360e016aa3d4c0d701e3fcde873
Stefan Hagen <stefan@hagen.link>
2023-01-01 16:16:39 +0100

    Synced version

--- medlar/__init__.py
+++ medlar/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.11.28+parent.63423cd2'
-# [[[end]]] (checksum: d6cbb972a54f39fd72aca87bc1189801)
+__version__ = '2023.1.1+parent.1e7e582d'
+# [[[end]]] (checksum: 4a8833329674c89a7222cf390f7e21b8)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
