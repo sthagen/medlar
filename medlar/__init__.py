1e7e582d97f05e58746ea24d20a353a890cc4023
Stefan Hagen <stefan@hagen.link>
2023-01-01 16:16:09 +0100

    Prepared version harness for implementation

--- medlar/__init__.py
+++ medlar/__init__.py
@@ -1,1 +1,6 @@
-
+# [[[fill git_describe()]]]
+__version__ = '2022.11.28+parent.63423cd2'
+# [[[end]]] (checksum: d6cbb972a54f39fd72aca87bc1189801)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
