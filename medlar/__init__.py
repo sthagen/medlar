660c85dd58c0ea61653ce3e7311a4c6e54ea92a3
Stefan Hagen <stefan@hagen.link>
2023-10-21 21:04:47 +0200

    Bumped impl version for release 2023.10.21

--- medlar/__init__.py
+++ medlar/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2023.1.1+parent.1e7e582d'
-# [[[end]]] (checksum: 4a8833329674c89a7222cf390f7e21b8)
+__version__ = '2023.10.21+parent.c437a22a'
+# [[[end]]] (checksum: 0db1dd3387429d961a06754a831c4dc2)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
